1a443710f455f2dbe0ad70d260c5f5549698dbc0
NPCYS <2200432033@qq.com>
2023-03-20 22:30:21 +0800

    idea第一次提交

--- src/main/java/com/mqz/hellogit/controller/HelloController.java
+++ src/main/java/com/mqz/hellogit/controller/HelloController.java
@@ -4,4 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    public void test(){
+        System.out.println("亻尔女子");
+    }
 }
