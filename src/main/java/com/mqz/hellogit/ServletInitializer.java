9fce73fcb04812836fea6a5e25a25351d4e6e89d
NPCYS <2200432033@qq.com>
2023-03-20 21:55:07 +0800

    idea第一次提交

---
+++ src/main/java/com/mqz/hellogit/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.mqz.hellogit;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HelloGitApplication.class);
+    }
+
+}
---
+++ src/main/java/com/mqz/hellogit/controller/HelloController.java
@@ -0,0 +1,4 @@
+package com.mqz.hellogit.controller;
+
+public class HelloController {
+}
